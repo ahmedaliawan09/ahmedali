cb79637bb6d23e4ed60cbc73aaf494a621c9b2c7
ahmedaliawan09 <ahmedawan81151@gmail.com>
2024-03-05 01:08:02 +0500

    changed file name

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 let iconn= document.getElementById("iconn");
 let nav=document.getElementById("12");
 let po=document.getElementById("po");
+let bacc=document.getElementById("bacc");
  iconn.onclick=function(){
     document.body.classList.toggle("dark");
     if(document.body.classList.contains("dark")){
         iconn.src="moon.png"
         nav.style.backgroundColor ='turquoise';
         po.style.color = 'black';
+        bacc.style.backgroundColor = 'turquoise';
     }
     else {
         iconn.src="brightness.png"
